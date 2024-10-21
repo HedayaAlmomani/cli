d37ed8d4b08bacb80f3f4bb8b942c1fed4a43dd1
hedaia almomani <hedaia.almomani@digitinary.com>
2024-10-21 13:40:18 +0300

    edit

--- stetper-cli/bin/cli.js
+++ stetper-cli/bin/cli.js
@@ -40,68 +40,162 @@ const stepperPageFilePath = path.join(targetFolder, "stepperPage", "index.tsx");
 
 const configs = [
   {
-    folderName: "FirstForm",
+    folderName: "RegistrationForm",
     content: formTemplate,
     formConfig: [
       {
-        name: "firstName",
+        name: "fullName",
         field: "input",
         type: "string",
       },
       {
-        name: "number",
+        name: "age",
         field: "input",
         type: "number",
       },
       {
-        name: "account",
+        name: "userType",
         field: "select",
         type: "string",
       },
       {
-        name: "mobileNumber",
+        name: "contactNumber",
         field: "phone",
         type: "string",
       },
       {
-        name: "document",
+        name: "profilePicture",
         field: "upload",
         type: "file",
       },
     ],
   },
   {
-    folderName: "SecondForm",
+    folderName: "FeedbackForm",
     content: formTemplate,
     formConfig: [
       {
-        name: "firstName",
+        name: "customerName",
         field: "input",
         type: "string",
       },
       {
-        name: "number",
+        name: "rating",
         field: "input",
         type: "number",
       },
       {
-        name: "account",
+        name: "serviceUsed",
         field: "select",
         type: "string",
       },
       {
-        name: "mobileNumber",
+        name: "emailAddress",
+        field: "input",
+        type: "string",
+      },
+      {
+        name: "additionalComments",
+        field: "upload",
+        type: "file",
+      },
+    ],
+  },
+  {
+    folderName: "OrderForm",
+    content: formTemplate,
+    formConfig: [
+      {
+        name: "customerName",
+        field: "input",
+        type: "string",
+      },
+      {
+        name: "quantity",
+        field: "input",
+        type: "number",
+      },
+      {
+        name: "productSelection",
+        field: "select",
+        type: "string",
+      },
+      {
+        name: "contactNumber",
         field: "phone",
         type: "string",
       },
       {
-        name: "document",
+        name: "orderDocument",
+        field: "upload",
+        type: "file",
+      },
+    ],
+  },
+  {
+    folderName: "SupportTicketForm",
+    content: formTemplate,
+    formConfig: [
+      {
+        name: "issueTitle",
+        field: "input",
+        type: "string",
+      },
+      {
+        name: "severityLevel",
+        field: "input",
+        type: "number",
+      },
+      {
+        name: "ticketCategory",
+        field: "select",
+        type: "string",
+      },
+      {
+        name: "customerPhone",
+        field: "phone",
+        type: "string",
+      },
+      {
+        name: "supportDocument",
+        field: "upload",
+        type: "file",
+      },
+    ],
+  },
+  {
+    folderName: "AppointmentForm",
+    content: formTemplate,
+    formConfig: [
+      {
+        name: "patientName",
+        field: "input",
+        type: "string",
+      },
+      {
+        name: "age",
+        field: "input",
+        type: "number",
+      },
+      {
+        name: "appointmentType",
+        field: "select",
+        type: "string",
+      },
+      {
+        name: "patientContact",
+        field: "phone",
+        type: "string",
+      },
+      {
+        name: "medicalRecords",
         field: "upload",
         type: "file",
       },
     ],
   },
 ];
+
 
 async function main() {
   try {
